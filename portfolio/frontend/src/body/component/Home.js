e86209439341a0827955f41e970fe76763d05f04
Arun Pottekat <apottekat@gmail.com>
2019-06-25 10:59:38 +1000

    Filtered the project structure

--- portfolio/frontend/src/body/component/Home.js
+++ portfolio/frontend/src/body/component/Home.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import { Jumbotron, Container } from 'react-bootstrap';
+import Container from '@material-ui/core/Container';
 
 class Home extends Component {
 	render() {
 		return (
             <div>
-                <Jumbotron fluid>
-                    <Container>
-                        <h1>Arun Pottekat</h1>
-                        <p>
-                            Something magical is soon about to come ...
-                            <br />
-                            For the time being, why not read some <strong>blog posts</strong>
-                        </p>
-                    </Container>
-                </Jumbotron>
+                <Container maxWidth="md">
+                    <h1>Arun Pottekat</h1>
+                    <p>
+                        Something magical is soon about to come ...
+                        <br />
+                        For the time being, why not read some <strong>blog posts</strong>
+                    </p>
+                </Container>
             </div>
 		);
 	}
