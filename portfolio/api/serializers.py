269c88a8a172f816325d15e4b191b1480b4cca65
Arun Pottekat <apottekat@gmail.com>
2019-07-09 12:23:47 +0530

    Added admin authorization

--- portfolio/api/serializers.py
+++ portfolio/api/serializers.py
@@ -31,9 +31,8 @@ class UserTokenSerializer(serializers.ModelSerializer):
     token = serializers.SerializerMethodField()
 
     def get_token(self, instance):
-        token_url = 'http://localhost:8000/o/token/'
-
         if os.environ.get("ENVIRONMENT") == "develop":
+            token_url = 'http://localhost:8000/o/token/'
             request_data = {
                 'client_id': '73T6z658kOeQ4IKe27DGJyhQAdDzr0ptiKF69kLT',
                 'client_secret': 'DX6uUhT72Bur4wNDWHWszoFsf3jqTI2BK9bHHk0OlXr5U10yFz4bOl0wAs89RXyg49cvQx1kT1RCUdfmD4wToQURsE6DMRg8XwCqRQJxjUYawYVqZv9N9lbvH5KHeeSb',
@@ -43,6 +42,7 @@ class UserTokenSerializer(serializers.ModelSerializer):
                 'grant_type': 'password'
             }
         else:
+            token_url = 'http://www.arunpottekat.me/o/token/'
             request_data = {
                 'client_id': 'kC2rHP64JJE7IKOZy0lCBJqyAsnrYEfXUXubBRYc',
                 'client_secret': '9iYnEMjc3Egnb9Pfz9wpizT5sg6E80XPT23D2XlcSPh7XAPOFAS5gPzGtaRQUma3X0kgJ1WCaEuypG59jzoWLW1ISrGkVfnI8W2YNSk0iR1UETKJW1ChVbpfd2MUq0c1',
