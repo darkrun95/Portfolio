7540f82cc751d4974d8a837d9a86b4dc2a6aab91
Arun Pottekat <pottekatarun1995@gmail.com>
2019-10-15 15:14:19 +1100

    Changed ElasticBeanstalk configurations

--- portfolio/frontend/webpack.prod.config.js
+++ portfolio/frontend/webpack.prod.config.js
@@ -14,7 +14,7 @@ config.plugins = config.plugins.concat([
     new webpack.DefinePlugin({
         'process.env': {
             'NODE_ENV': JSON.stringify('production'),
-            'BASE_API_URL': JSON.stringify('http://www.arunpottekat.me/'),
+            'BASE_API_URL': JSON.stringify('https://arunpottekat.me/'),
         }
     }),
 
